3aa2373eb7cbc754d9b139b23a22e60c372a1047
gaurav-cpp-dev <goluuni874@gmail.com>
2026-01-04 17:28:02 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,30 @@
 using namespace std;
 
 int main() {
-    cout << "Student Management System started" << endl;
+    int choice;
+
+    while (true) {
+        cout << "\n===== Student Management System =====" << endl;
+        cout << "1. Add Student" << endl;
+        cout << "2. View Students" << endl;
+        cout << "3. Exit" << endl;
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        if (choice == 1) {
+            cout << "Add Student selected" << endl;
+        }
+        else if (choice == 2) {
+            cout << "View Students selected" << endl;
+        }
+        else if (choice == 3) {
+            cout << "Exiting program..." << endl;
+            break;
+        }
+        else {
+            cout << "Invalid choice!" << endl;
+        }
+    }
+
     return 0;
 }
